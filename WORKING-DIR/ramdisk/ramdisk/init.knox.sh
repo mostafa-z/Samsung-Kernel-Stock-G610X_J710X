e8a2ad6480411e1f69d75525153bd092b9b0fca7
mostafa-z <mostafazarghami@gmail.com>
2017-07-16 18:58:16 +0430

    ramdisk:knox: add related samsung flags thanks @Tkkg1994

--- WORKING-DIR/ramdisk/ramdisk/init.knox.sh
+++ WORKING-DIR/ramdisk/ramdisk/init.knox.sh
@@ -26,11 +26,16 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Set Knox flag to 0x0
-/sbin/resetprop -v -n ro.boot.warranty_bit 0
-/sbin/resetprop -v -n ro.warranty_bit 0
+/sbin/resetprop -n ro.boot.warranty_bit "0"
+/sbin/resetprop -n ro.warranty_bit "0"
 
 # Fix some safetynet flags
 /sbin/resetprop -n ro.boot.veritymode "enforcing"
 /sbin/resetprop -n ro.boot.verifiedbootstate "green"
 /sbin/resetprop -n ro.boot.flash.locked "1"
 /sbin/resetprop -n ro.boot.ddrinfo "00000001"
+
+# Samsung related flags
+/sbin/resetprop -n ro.fmp_config "1"
+/sbin/resetprop -n ro.boot.fmp_config "1"
+/sbin/resetprop -n sys.oem_unlock_allowed "0"
